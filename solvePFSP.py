e3cd94f677c175666026d16dccdef7efb62897a5
Luana Marrocco <luanamarrocco@MacBook-Pro-de-Luana.local>
2019-05-31 17:19:06 +0200

    reading the instance

--- solvePFSP.py
+++ solvePFSP.py
@@ -1,10 +1,12 @@
 fileName = "PFSP_instances/DD_Ta051.txt"
 M = 0
 N = 0
-processesTime = []
+processingTime = [] #N lines, M colums
+dueDates = []
+weights = []
 
 def readFile(filename):
-	global M,N, processesTime
+	global M,N, processesTime, dueDates, weights
 	f=open(filename, "r")
 	lines = f.readlines()
 	i = 0
@@ -12,12 +14,15 @@ def readFile(filename):
 		if(i == 0):
 			N,M = list(map(int, line.split()))
 			i = i+1
-		elif(i <= M):
-			processesTime.append(list(map(int, line.split())))
-
+		elif(i <= N):
+			processingTime.append(list(map(int, line.split())))
 			i += 1
+		else:
+			content = line.split()
+			if(content[0] != "Reldue"):
+				_,date,_,weight = list(map(int, content))
+				dueDates.append(date)
+				weights.append(weight)
 
 readFile(fileName)
-for elem in processesTime:
-	print(elem)
 
